e17b2b7725baf6da66c416accb724d2124912291
sherry <sherry.kisilu@studebnt.moringaschool.com>
2022-09-08 14:23:49 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form=document.getElementById("create-task-form")
+ form.addEventListener("submit",function(event){
+  event.preventDefault();
+  displayData(event.target.new_task_description.value)
+  form.reset()
+ })
+function displayData(task){
+const input=document.getElementById("tasks");
+const data=document.createElement('li');
+const button=document.createElement("button")
+button.addEventListener("click",function(event){
+event.target.parentNode.remove();
+})
+data.textContent=`${task}`;
+button.textContent=" x ";
+data.appendChild(button);
+input.appendChild(data);
+}
 });
